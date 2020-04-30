e3f27a184e17920373c56c52ae137eb88dc8b796
Ham-ZZi <hyunjia2@naver.com>
2020-04-30 11:06:35 +0900

    Initial commit

---
+++ app/src/main/java/com/example/consumerweiez/intro.java
@@ -0,0 +1,15 @@
+package com.example.consumerweiez;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class intro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+    }
+}
