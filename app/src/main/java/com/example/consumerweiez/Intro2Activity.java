6b0ef0d218952e0ddace598c69f504ed7e8660de
Ham-ZZi <hyunjia2@naver.com>
2020-04-30 16:03:34 +0900

    수정 2020.04.30 16시

--- app/src/main/java/com/example/consumerweiez/Intro2Activity.java
+++ app/src/main/java/com/example/consumerweiez/Intro2Activity.java
@@ -2,13 +2,28 @@ package com.example.consumerweiez;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Intro2Activity extends AppCompatActivity {
+
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro2);
+
+        loginButton = findViewById(R.id.loginBtn1);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intro2Activity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
